373597ef2bbcbda4bda7bf2af009b2031aa5a63b
Stefan Dierauf <sdierauf@gmail.com>
2016-05-02 11:59:28 +0200

    actually done with ex2 now

--- convnet/l1LossBackward.m
+++ convnet/l1LossBackward.m
@@ -1,5 +1,4 @@
 function dx = l1LossBackward(x,r,p)
-% TODO: Replace the following line with your implementation
-dx = rand(size(x), 'like', x) ;
-
+% TODO: Replace the following line with your implementatio
+dx = p * sign(x - r);
 dx = dx / (size(x,1) * size(x,2)) ;  % normalize by image size
--- convnet/l1LossForward.m
+++ convnet/l1LossForward.m
@@ -1,5 +1,5 @@
 function y = l1LossForward(x,r)
 % TODO: Replace the following line with your implementation
-y = rand(size(x), 'like', x) ;
-
+delta = abs(x - r);
+y = sum(delta(:));
 y = y / (size(x,1) * size(x,2)) ;  % normalize by image size
